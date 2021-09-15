96b78738a1ee4b4032feefeab5f706f4f7dd4bae
Greds <vyacheslav-98@yandex.ru>
2021-09-15 14:57:27 +0300

    Transfer

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int cash = 2_000_000_000;
+        int transfer = 500_000_000;
+        int result = cash + transfer;
+        System.out.println(result);
+    }
+}
